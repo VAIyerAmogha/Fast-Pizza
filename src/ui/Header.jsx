4e8e694846c8f8b2775edacc4b4f72a48c264ade
Amogha V A <amogha.va.iyer@gmail.com>
2025-03-03 18:24:01 +0530

    Added AppLayout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Co.</Link>
+      <p>Header</p>
+    </header>
+  );
+}
+
+export default Header;
