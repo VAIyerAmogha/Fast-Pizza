8eebc5c1eab42356f433ba61326886807e9086c9
Amogha V A <amogha.va.iyer@gmail.com>
2025-03-04 23:30:41 +0530

    Added styling to menu

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const clsName =
-    "text-semibold duration-300ms inline-block rounded-full bg-yellow-400 px-4 py-3 uppercase text-stone-800 transition-colors hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+function Button({ children, disabled, to, type }) {
+  const base =
+    "text-semibold duration-300ms inline-block rounded-full bg-yellow-400 uppercase text-stone-800 transition-colors hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+
+  const styles = {
+    primary: base + " px-4  py-3 sm:px-6 sm:py-4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-sm",
+  };
   if (to) {
     return (
-      <Link to={to} className={clsName}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   }
   return (
-    <button className={clsName} disabled={disabled}>
+    <button className={styles[type]} disabled={disabled}>
       {children}
     </button>
   );
