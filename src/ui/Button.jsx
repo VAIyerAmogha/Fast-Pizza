953054d090876be5df24b5795ce66a215bd81873
Amogha V A <amogha.va.iyer@gmail.com>
2025-03-05 15:32:37 +0530

    Added styling to orders Pages

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, type }) {
   const base =
-    "text-semibold duration-300ms inline-block rounded-full bg-yellow-400 uppercase text-stone-800 transition-colors hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+    "text-semibold text-sm duration-300ms inline-block rounded-full bg-yellow-400 uppercase text-stone-800 transition-colors hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
   const styles = {
     primary: base + " px-4  py-3 sm:px-6 sm:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-sm",
     secondary:
-      "text-semibold duration-300ms inline-block rounded-full border-2 border-stone-300 uppercase text-stone-400 transition-colors hover:bg-stone-300 hover:text-stone-700 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed  px-4  py-2.5 sm:px-6 sm:py-3.5",
+      "text-semibold text-sm duration-300ms inline-block rounded-full border-2 border-stone-300 uppercase text-stone-400 transition-colors hover:bg-stone-300 hover:text-stone-700 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed  px-4  py-2.5 sm:px-6 sm:py-3.5",
   };
   if (to) {
     return (
