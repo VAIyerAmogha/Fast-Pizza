f795a55602678caf2b1199e59170495cebddcd76
Amogha V A <amogha.va.iyer@gmail.com>
2025-03-05 21:08:46 +0530

    Added Cart functionality

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,6 +1,12 @@
+import { useSelector } from "react-redux";
+
 function UserName() {
+  const userName = useSelector((state) => state.user.username);
+  if (!userName) {
+    return;
+  }
   return (
-    <div className="hidden text-sm font-semibold md:block">Amogha V A</div>
+    <div className="hidden text-sm font-semibold md:block">{userName}</div>
   );
 }
 
